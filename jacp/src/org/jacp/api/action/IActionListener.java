5cff0a80b641d880854d97853e21a1b671de2d8f
amo.ahcp <amo.ahcp@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2010-05-09 17:37:33 +0000

    package structure refactored

---
+++ jacp/src/org/jacp/api/action/IActionListener.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jacp.api.action;
+
+/**
+ * handles implementation specific ActionListener L defines the basic listener
+ * type E defines the basic action type M defines the basic message type
+ * 
+ * @author Andy Moncsek
+ */
+public interface IActionListener<L, E, M> {
+
+	/**
+	 * notify component when action fired
+	 * 
+	 * @param action
+	 */
+	public abstract void notifyComponents(IAction<M, E> action);
+
+	/**
+	 * set Action to listener
+	 * 
+	 * @param action
+	 */
+	public abstract void setAction(IAction<M, E> action);
+
+	/**
+	 * returns the action
+	 * 
+	 * @return
+	 */
+	public IAction<M, E> getAction();
+
+	/**
+	 * returns implemetation specific ActionListener
+	 * 
+	 * @return
+	 */
+	public abstract L getListener();
+}
