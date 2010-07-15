2fb39429598ad04dc227eecd97c1e418eed4577b
amo.ahcp <amo.ahcp@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2010-07-15 19:28:03 +0000

    ThradLocal access added

--- trunk/jacp/srcSwing/org/jacp/swing/rcp/componentLayout/SwingPerspectiveLayout.java
+++ trunk/jacp/srcSwing/org/jacp/swing/rcp/componentLayout/SwingPerspectiveLayout.java
@@ -25,6 +25,10 @@ public class SwingPerspectiveLayout implements
 	private final boolean scrollable = true;
 	private boolean replaceMode = true;
 	private final Map<String, Container> targetComponents = new HashMap<String, Container>();
+	private final ThreadLocal<Map<String, Container>> tComponents = new ThreadLocal<Map<String, Container>>() 
+	  { 
+	    @Override protected Map<String, Container> initialValue() { return targetComponents; } 
+	  }; 
 
 	public SwingPerspectiveLayout() {
 	}
@@ -61,7 +65,7 @@ public class SwingPerspectiveLayout implements
 
 	@Override
 	public Map<String, Container> getTargetLayoutComponents() {
-		return targetComponents;
+		return tComponents.get();
 	}
 
 }
