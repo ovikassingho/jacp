2435916c1fb5f8b122d8b93b654a54e524f50f62
amo.ahcp <amo.ahcp@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2010-07-18 17:48:21 +0000

    background component interface (IBGComponent) added; initialisation enhanced

--- trunk/jacp/srcSwing/org/jacp/swing/rcp/util/StateComponentInitWorker.java
+++ trunk/jacp/srcSwing/org/jacp/swing/rcp/util/StateComponentInitWorker.java
@@ -4,37 +4,48 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import org.jacp.api.action.IAction;
-import org.jacp.api.component.ISubComponent;
+import org.jacp.api.component.IBGComponent;
+import org.jacp.swing.rcp.action.SwingAction;
 
 public class StateComponentInitWorker
 		extends
-		AbstractComponentWorker<ISubComponent<ActionListener, ActionEvent, Object>> {
+		AbstractComponentWorker<IBGComponent<ActionListener, ActionEvent, Object>> {
 
-	private final ISubComponent<ActionListener, ActionEvent, Object> component;
+	private final IBGComponent<ActionListener, ActionEvent, Object> component;
 	private final IAction<ActionEvent, Object> action;
 
 	public StateComponentInitWorker(
-			final ISubComponent<ActionListener, ActionEvent, Object> component,
+			final IBGComponent<ActionListener, ActionEvent, Object> component,
 			final IAction<ActionEvent, Object> action) {
 		this.component = component;
 		this.action = action;
 	}
 
 	@Override
-	protected ISubComponent<ActionListener, ActionEvent, Object> runHandleSubcomponent(
-			final ISubComponent<ActionListener, ActionEvent, Object> component,
+	protected IBGComponent<ActionListener, ActionEvent, Object> runHandleSubcomponent(
+			final IBGComponent<ActionListener, ActionEvent, Object> component,
 			final IAction<ActionEvent, Object> action) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected ISubComponent<ActionListener, ActionEvent, Object> doInBackground()
+	protected IBGComponent<ActionListener, ActionEvent, Object> doInBackground()
 			throws Exception {
-		synchronized (component) {
-			component.handle(action);
+		final IBGComponent<ActionListener, ActionEvent, Object> comp = component;
+		synchronized (comp) {
+			final Object value = comp.handle(action);
+			final String targetId = comp.getHandleComponentTarget();
+			if (value != null && targetId != null) {
+				IAction<ActionEvent, Object> action2 = new SwingAction(comp
+						.getId(), targetId, value);
+				// TODO delegate directly to observer !!
+				comp.getActionListener().setAction(action2);
+				comp.getActionListener().getListener().actionPerformed(
+						action.getActionEvent());
+			}
 		}
-		return component;
+		return comp;
 	}
 
 }
