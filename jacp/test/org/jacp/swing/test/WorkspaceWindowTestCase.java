b3dd8adc62dccdc2b78490ee6b431693269de658
afreakm <afreakm@gmx.de>
2011-03-23 19:30:07 +0000

    UnitTest Extended

--- jacp/test/org/jacp/swing/test/WorkspaceWindowTestCase.java
+++ jacp/test/org/jacp/swing/test/WorkspaceWindowTestCase.java
@@ -3,146 +3,346 @@ package org.jacp.swing.test;
 import java.awt.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
+
+import javax.swing.JSplitPane;
 
 import org.jacp.swing.rcp.util.OSXBottomBarPanel;
 import org.jacp.swing.rcp.util.OSXToolBar;
 import org.jacp.swing.test.main.UnitTestBenchMain;
+import org.uispec4j.Button;
 import org.uispec4j.MenuBar;
 import org.uispec4j.MenuItem;
 import org.uispec4j.UISpecTestCase;
 import org.uispec4j.Window;
+import org.uispec4j.assertion.Assertion;
+import org.uispec4j.finder.ComponentMatcher;
 import org.uispec4j.interception.MainClassAdapter;
-
-
 
 public class WorkspaceWindowTestCase extends UISpecTestCase {
 	protected void setUp() throws Exception {
-	    super.setUp();
-	    setAdapter(new MainClassAdapter(UnitTestBenchMain.class));
+		super.setUp();
+		setAdapter(new MainClassAdapter(UnitTestBenchMain.class));
 	}
-	
+
 	public void testGetWindow() {
-		 Window window = getMainWindow();
-		 assertNotNull(window);
+		Window window = getMainWindow();
+		assertNotNull(window);
 	}
-	
+
 	public void testGetMenue() throws InterruptedException {
-		 Window window = getMainWindow();
-		 //Asynchrony behavior
-		 Thread.sleep(500);
-		 assertTrue(window.containsMenuBar().isTrue());
+		Window window = getMainWindow();
+		// Asynchrony behavior
+		Thread.sleep(500);
+		assertTrue(window.containsMenuBar().isTrue());
 	}
-	
+
 	public void testGetBars() throws InterruptedException {
 		// bottom bar
-		 Window window = getMainWindow();	
-		 //Asynchrony behavior
-		 Thread.sleep(500);
-		 Component[] resultBottom = window.getSwingComponents(org.jacp.swing.rcp.util.OSXBottomBarPanel.class);
-		 assertNotNull(resultBottom);
-		 assertTrue(resultBottom.length==1);
-		 
-		 
-		 // tool bar
-		 Component[] result = window.getSwingComponents(org.jacp.swing.rcp.util.OSXToolBar.class);
-		 assertNotNull(result);
-		 assertTrue(result.length==1);
-		 
-		
+		Window window = getMainWindow();
+		// Asynchrony behavior
+		Thread.sleep(500);
+		Component[] resultBottom = window
+				.getSwingComponents(org.jacp.swing.rcp.util.OSXBottomBarPanel.class);
+		assertNotNull(resultBottom);
+		assertTrue(resultBottom.length == 1);
+
+		// tool bar
+		Component[] result = window
+				.getSwingComponents(org.jacp.swing.rcp.util.OSXToolBar.class);
+		assertNotNull(result);
+		assertTrue(result.length == 1);
+
 	}
-	
+
 	public void testGetMenuEntries() throws InterruptedException {
 		Window window = getMainWindow();
-		 //Asynchrony behavior
-		 Thread.sleep(500);
+		// Asynchrony behavior
+		Thread.sleep(500);
 		MenuBar menuBar = window.getMenuBar();
 		assertNotNull(menuBar);
 		MenuItem menuPerspectiveOne = menuBar.getMenu("perspectiveOne");
 		assertNotNull(menuPerspectiveOne);
 		assertNotNull(menuPerspectiveOne.getSubMenu("PerspectiveOne"));
-		
+
 		MenuItem menuPerspectiveTwo = menuBar.getMenu("perspectiveTwo");
 		assertNotNull(menuPerspectiveTwo);
 		assertNotNull(menuPerspectiveTwo.getSubMenu("perspectiveTwo"));
-		
+
 		MenuItem menuPerspectiveThree = menuBar.getMenu("perspectiveThree");
 		assertNotNull(menuPerspectiveThree);
 		assertNotNull(menuPerspectiveThree.getSubMenu("PerspectiveThree"));
 	}
-	
+
 	public void testGetPerspectiveToolBarEntries() throws InterruptedException {
 		Window window = getMainWindow();
-		 //Asynchrony behavior
-		 Thread.sleep(500);
-		 // tool bar
-		 Component[] result = window.getSwingComponents(org.jacp.swing.rcp.util.OSXToolBar.class);
-		 assertNotNull(result);
-		 assertTrue(result.length==1);
-		 Component toolBar = result[0];
-		 assertNotNull(toolBar);
-		 String[] perspectiveButtons={"PerspectiveOneToolBarButton","PerspectiveTwoToolBarButton","PerspectiveThreeToolBarButton"};
-		 if(toolBar instanceof OSXToolBar) {
-			 Component[] buttons = ((OSXToolBar)toolBar).getComponents();
-			 assertNotNull(buttons);
-			 assertFalse(buttons.length==0);
-			 List<String> errorList = new ArrayList<String>( Arrays.asList(perspectiveButtons));
+		// Asynchrony behavior
+		Thread.sleep(500);
+		// tool bar
+		Component[] result = window
+				.getSwingComponents(org.jacp.swing.rcp.util.OSXToolBar.class);
+		assertNotNull(result);
+		assertTrue(result.length == 1);
+		Component toolBar = result[0];
+		assertNotNull(toolBar);
+		Assertion assertion = window.containsComponent(new ComponentMatcher() {
 
-			 for(int i=0;i<buttons.length;i++){
-				 Component button = buttons[i];
-				 System.out.println(button.getName());
-				 assertNotNull(button);
-				 if(!contains(perspectiveButtons, button.getName())){ errorList.remove(button.getName()+"");}
-			 }
-			 assertTrue(errorList.isEmpty());
-			 
-		 } else {
-			 assertTrue(false);
-		 }
-		 
-		
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("PerspectiveOneToolBarButton") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("PerspectiveTwoToolBarButton") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("PerspectiveThreeToolBarButton") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
 	}
-	
-	public void testGetPerspectiveBottomBarEntries() throws InterruptedException {
+
+	public void testGetPerspectiveBottomBarEntries()
+			throws InterruptedException {
 		Window window = getMainWindow();
-		 //Asynchrony behavior
-		 Thread.sleep(500);
-		 // tool bar
-		 Component[] result = window.getSwingComponents(org.jacp.swing.rcp.util.OSXBottomBarPanel.class);
-		 assertNotNull(result);
-		 assertTrue(result.length==1);
-		 Component toolBar = result[0];
-		 assertNotNull(toolBar);
-		 String[] perspectiveButtons={"PerspectiveOneBottomBarButton","PerspectiveTwoBottomBarButton","PerspectiveThreeBottomBarButton"};
-		 if(toolBar instanceof OSXBottomBarPanel) {
-			 Component[] buttons = ((OSXBottomBarPanel)toolBar).getComponents();
-			 assertNotNull(buttons);
-			 assertFalse(buttons.length==0);
-			 List<String> errorList = new ArrayList<String>( Arrays.asList(perspectiveButtons));
+		// Asynchrony behavior
+		Thread.sleep(500);
+		// tool bar
+		Component[] result = window
+				.getSwingComponents(org.jacp.swing.rcp.util.OSXBottomBarPanel.class);
+		assertNotNull(result);
+		assertTrue(result.length == 1);
+		Component toolBar = result[0];
+		assertNotNull(toolBar);
+		Assertion assertion = window.containsComponent(new ComponentMatcher() {
 
-			 for(int i=0;i<buttons.length;i++){
-				 Component button = buttons[i];
-				 System.out.println(button.getName());
-				 assertNotNull(button);
-				 if(!contains(perspectiveButtons, button.getName())){ errorList.remove(button.getName()+"");}
-			 }
-			 assertTrue(errorList.isEmpty());
-			 
-		 } else {
-			 assertTrue(false);
-		 }
-		 
-		
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("PerspectiveOneBottomBarButton") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("PerspectiveTwoBottomBarButton") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("PerspectiveThreeBottomBarButton") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+
 	}
-	
-	
+
+	public void testPerspectiveOneBasicUIFunctionsIsUIObjectSIncluded()
+			throws InterruptedException {
+		// bottom bar
+		Window window = getMainWindow();
+		// Asynchrony behavior
+		Thread.sleep(1000);
+		Assertion assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("splitPanePerspectiveOne") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("panelOnePerspectiveOne") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("panelTwoPerspectiveOne") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("ButtonOnePerspectiveOneLEFT") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("BottonTwoPerspectiveOneRIGHT") : false;
+			}
+		});
+	}
+
+	public void testPerspectiveOneBasicUIAndLocalMessageFunction()
+			throws InterruptedException {
+		// bottom bar
+		Window window = getMainWindow();
+		// Asynchrony behavior
+		Thread.sleep(1000);
+		Assertion assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("splitPanePerspectiveOne") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		Button buttonOne = window.getButton("PerspectiveOneToolBarButton");
+		// ////////NEGATIVE TEST 1 //////////////////
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("ButtonOnePerspectiveOneTOP") : false;
+			}
+		});
+		assertFalse(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("BottonTwoPerspectiveOneBOTTOM") : false;
+			}
+		});
+		assertFalse(assertion.isTrue());
+		// ////////NEGATIVE TEST 1 END //////////////////
+
+		// //////////POSITIVE TEST 1 /////////////////////////
+		buttonOne.click();
+		// Asynchrony behavior
+		Thread.sleep(500);
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("ButtonOnePerspectiveOneTOP") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("BottonTwoPerspectiveOneBOTTOM") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		// ////////POSITIVE TEST 1 END //////////////////
+
+		// ////////NEGATIVE TEST 2 //////////////////
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("ButtonOnePerspectiveOneLEFT") : false;
+			}
+		});
+		assertFalse(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("BottonTwoPerspectiveOneRIGHT") : false;
+			}
+		});
+		assertFalse(assertion.isTrue());
+		// ////////NEGATIVE TEST 2 END //////////////////
+		Button buttonTwo = window.getButton("PerspectiveOneBottomBarButton");
+		buttonTwo.click();
+		// Asynchrony behavior
+		Thread.sleep(500);
+		// //////////POSITIVE TEST 2 /////////////////////////
+
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("ButtonOnePerspectiveOneLEFT") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("BottonTwoPerspectiveOneRIGHT") : false;
+			}
+		});
+		assertTrue(assertion.isTrue());
+		// ////////POSITIVE TEST 2 END //////////////////
+		
+		// ////////NEGATIVE TEST 3 //////////////////
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("ButtonOnePerspectiveOneTOP") : false;
+			}
+		});
+		assertFalse(assertion.isTrue());
+		assertion = window.containsComponent(new ComponentMatcher() {
+
+			@Override
+			public boolean matches(Component component) {
+				return component.getName() != null ? component.getName()
+						.equals("BottonTwoPerspectiveOneBOTTOM") : false;
+			}
+		});
+		assertFalse(assertion.isTrue());
+		// ////////NEGATIVE TEST 3 END //////////////////
+	}
+
 	private boolean contains(final String[] array, final String searchString) {
-		for(int i=0; i<array.length;i++) {
+		for (int i = 0; i < array.length; i++) {
 			String arrayString = array[i];
-			if(arrayString.toLowerCase().equals(searchString)) return true;
-		}		
+			if (arrayString.toLowerCase().equals(searchString))
+				return true;
+		}
 		return false;
 	}
 }
