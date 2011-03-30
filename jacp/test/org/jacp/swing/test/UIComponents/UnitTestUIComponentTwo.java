b361dee5308c256e8cd4424a1d196c9cc572fd97
afreakm <afreakm@gmx.de>
2011-03-30 17:32:57 +0000

    subcomponents for ui unit tests extended

--- jacp/test/org/jacp/swing/test/UIComponents/UnitTestUIComponentTwo.java
+++ jacp/test/org/jacp/swing/test/UIComponents/UnitTestUIComponentTwo.java
@@ -2,12 +2,15 @@ package org.jacp.swing.test.UIComponents;
 
 import java.awt.Container;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.jacp.api.action.IAction;
+import org.jacp.api.action.IActionListener;
 import org.jacp.api.componentLayout.Layout;
 import org.jacp.swing.rcp.component.ASwingComponent;
 /**
@@ -34,7 +37,14 @@ public class UnitTestUIComponentTwo extends ASwingComponent {
 		final JPanel panelEditorTwo = new JPanel();
 		final JButton buttonOneEditorTwo = new JButton("ButtonOneEditorTwo");
 		buttonOneEditorTwo.setName("ButtonOneEditorTwo");
+		final IActionListener<ActionListener, ActionEvent, Object> listenerBottomOne = getActionListener();
+		listenerBottomOne.getAction().addMessage("id001", "oneComponentTwo");
+		buttonOneEditorTwo.addActionListener(listenerBottomOne.getListener());
+		JTextField fieldTwo = new JTextField();
+		fieldTwo.setName("oneTextFiledComponentTwo");
+		fieldTwo.setText("messgae: "+action.getLastMessage()+ " from: "+action.getSourceId());
 		panelEditorTwo.add(buttonOneEditorTwo);
+		panelEditorTwo.add(fieldTwo);
 		return panelEditorTwo;
 	}
 }
