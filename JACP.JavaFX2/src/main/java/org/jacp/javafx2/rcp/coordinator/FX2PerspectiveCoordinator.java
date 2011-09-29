35208524c16b860d64e446c3f303a4b66c96aa62
afreakm <afreakm@gmx.de>
2011-09-29 14:55:44 +0000

    perspective coordinator and workbench initial implementation

--- JACP.JavaFX2/src/main/java/org/jacp/javafx2/rcp/coordinator/FX2PerspectiveCoordinator.java
+++ JACP.JavaFX2/src/main/java/org/jacp/javafx2/rcp/coordinator/FX2PerspectiveCoordinator.java
@@ -17,47 +17,144 @@
  */
 package org.jacp.javafx2.rcp.coordinator;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
+
 import org.jacp.api.action.IAction;
 import org.jacp.api.component.IComponent;
 import org.jacp.api.component.ISubComponent;
 import org.jacp.api.coordinator.IPerspectiveCoordinator;
 import org.jacp.api.perspective.IPerspective;
+import org.jacp.api.workbench.IWorkbench;
 
 /**
  * Observe perspective actions and delegates message to correct component
+ * 
  * @author Andy Moncsek
  */
-public class FX2PerspectiveCoordinator extends AFX2Coordinator  implements
-	IPerspectiveCoordinator<EventHandler<ActionEvent>, ActionEvent, Object> {
+public class FX2PerspectiveCoordinator extends AFX2Coordinator implements
+		IPerspectiveCoordinator<EventHandler<ActionEvent>, ActionEvent, Object> {
 
-    public void handleMessage(String id, IAction<ActionEvent, Object> action) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	private final IWorkbench<?, Node, EventHandler<ActionEvent>, ActionEvent, Object> workbench;
+	private List<IPerspective<EventHandler<ActionEvent>, ActionEvent, Object>> perspectives = new CopyOnWriteArrayList<IPerspective<EventHandler<ActionEvent>, ActionEvent, Object>>();
 
-    public void delegateMessage(String target, IAction<ActionEvent, Object> action) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	public FX2PerspectiveCoordinator(
+			final IWorkbench<?, Node, EventHandler<ActionEvent>, ActionEvent, Object> workbench) {
+		setDaemon(true);
+		this.workbench = workbench;
+	}
 
-    public <P extends IComponent<EventHandler<ActionEvent>, ActionEvent, Object>> void handleActive(P component, IAction<ActionEvent, Object> action) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	public void handleMessage(String target, IAction<ActionEvent, Object> action) {
+		final IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> perspective = getObserveableById(
+				getTargetPerspectiveId(target), perspectives);
+		if (perspective != null) {
+			final IAction<ActionEvent, Object> actionClone = getValidAction(
+					action, target, action.getMessageList().get(target));
+			handleComponentHit(target, actionClone, perspective);
+		} else {
+			// TODO implement missing perspective handling!!
+			throw new UnsupportedOperationException(
+					"No responsible perspective found. Handling not implemented yet.");
+		}
+	}
 
-    public <P extends IComponent<EventHandler<ActionEvent>, ActionEvent, Object>> void handleInActive(P component, IAction<ActionEvent, Object> action) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	/**
+	 * handle message target hit
+	 * 
+	 * @param target
+	 * @param action
+	 */
+	private void handleComponentHit(
+			final String target,
+			final IAction<ActionEvent, Object> action,
+			final IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> perspective) {
+		if (perspective.isActive()) {
+			handleMessageToActivePerspective(target, action, perspective);
+		} else {
+			// perspective was not active and will be initialized
+			log(" //1.1.1.1// perspective HIT handle IN-ACTIVE: "
+					+ action.getTargetId());
+			handleWorkspaceModeSpecific();
+			handleInActive(perspective, action);
+		}
+	}
 
-    public void delegateTargetChange(String target, ISubComponent<EventHandler<ActionEvent>, ActionEvent, Object> component) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	/**
+	 * handle message to active perspective; check if target is perspective or
+	 * component
+	 * 
+	 * @param target
+	 * @param action
+	 * @param perspective
+	 */
+	private void handleMessageToActivePerspective(
+			final String target,
+			final IAction<ActionEvent, Object> action,
+			final IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> perspective) {
+		// if perspective already active handle perspective and replace
+		// with newly created layout component in workbench
+		log(" //1.1.1.1// perspective HIT handle ACTIVE: "
+				+ action.getTargetId());
+		if (isLocalMessage(target)) {
+			// message is addressing perspective
+			handleWorkspaceModeSpecific();
+			handleActive(perspective, action);
+		} else {
+			// delegate to addressed component
+			perspective.delegateComponentMassege(target, action);
+		}
+	}
 
-    public void addPerspective(IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> perspective) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	/**
+	 * handle workspace mode specific actions. When working in single pain mode
+	 * all components are deactivated and only one (the active) will be
+	 * activated; when working in window mode all components must be visible
+	 */
+	private void handleWorkspaceModeSpecific() {
+		switch (workbench.getWorkbenchLayout().getWorkspaceMode()) {
+		case WINDOWED_PANE:
+			workbench.enableComponents();
+			break;
+		default:
+			workbench.disableComponents();
+		}
+	}
 
-    public void removePerspective(IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> perspective) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
+	public void delegateMessage(String target,
+			IAction<ActionEvent, Object> action) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public <P extends IComponent<EventHandler<ActionEvent>, ActionEvent, Object>> void handleActive(
+			P component, IAction<ActionEvent, Object> action) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public <P extends IComponent<EventHandler<ActionEvent>, ActionEvent, Object>> void handleInActive(
+			P component, IAction<ActionEvent, Object> action) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void delegateTargetChange(
+			String target,
+			ISubComponent<EventHandler<ActionEvent>, ActionEvent, Object> component) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void addPerspective(
+			IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> perspective) {
+		perspective.setObserver(this);
+		this.perspectives.add(perspective);
+	}
+
+	public void removePerspective(
+			IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> perspective) {
+		perspective.setObserver(null);
+		this.perspectives.remove(perspective);
+	}
+
 }
