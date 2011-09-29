54e8a0ec86b843d30b8ab43df9d925f8da6401f6
afreakm@gmx.de <afreakm@gmx.de@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2011-09-29 14:55:44 +0000

    perspective coordinator and workbench initial implementation

--- trunk/JACP.JavaFX2/src/main/java/org/jacp/javafx2/rcp/workbench/AFX2Workbench.java
+++ trunk/JACP.JavaFX2/src/main/java/org/jacp/javafx2/rcp/workbench/AFX2Workbench.java
@@ -17,10 +17,160 @@
  */
 package org.jacp.javafx2.rcp.workbench;
 
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.jacp.api.action.IAction;
+import org.jacp.api.component.IRootComponent;
+import org.jacp.api.componentLayout.IWorkbenchLayout;
+import org.jacp.api.componentLayout.Layout;
+import org.jacp.api.coordinator.IPerspectiveCoordinator;
+import org.jacp.api.launcher.Launcher;
+import org.jacp.api.perspective.IPerspective;
+import org.jacp.api.workbench.IWorkbench;
+import org.jacp.javafx2.rcp.componentLayout.FX2WorkbenchLayout;
+import org.jacp.javafx2.rcp.coordinator.FX2PerspectiveCoordinator;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
 /**
- *
+ * represents the basic JavaFX2 workbench instance; handles perspectives and
+ * components;
+ * 
  * @author Andy Moncsek
  */
-public class AFX2Workbench {
-    
+public class AFX2Workbench extends Application
+		implements
+		IWorkbench<Region, Node, EventHandler<ActionEvent>, ActionEvent, Object>,
+		IRootComponent<IPerspective<EventHandler<ActionEvent>, ActionEvent, Object>, IAction<ActionEvent, Object>> {
+
+	private List<IPerspective<EventHandler<ActionEvent>, ActionEvent, Object>> perspectives;
+	private final IPerspectiveCoordinator<EventHandler<ActionEvent>, ActionEvent, Object> perspectiveHandler = new FX2PerspectiveCoordinator(this);
+	private final int inset = 50;
+	private final IWorkbenchLayout<Region, Node> layout = new FX2WorkbenchLayout();
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
+	private Launcher<?> launcher;
+	private MenuBar menu;
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+
+	}
+
+	@Override
+	public void setPerspectives(
+			List<IPerspective<EventHandler<ActionEvent>, ActionEvent, Object>> perspectives) {
+		this.perspectives = perspectives;
+
+	}
+
+	@Override
+	public List<IPerspective<EventHandler<ActionEvent>, ActionEvent, Object>> getPerspectives() {
+		return this.perspectives;
+	}
+
+	@Override
+	public void init(Launcher<?> launcher) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initWorkbenchMenu() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initMenuBar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Node getDefaultMenu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node handleMenuEntries(Node meuBar) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void handleBarEntries(Map<Layout, Node> bars) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disableComponents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enableComponents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleInitialLayout(IAction<ActionEvent, Object> action,
+			IWorkbenchLayout<Region, Node> layout) {
+		// TODO move to concrete workbench!!!
+
+	}
+
+	@Override
+	public IWorkbenchLayout<Region, Node> getWorkbenchLayout() {
+		return null;
+	}
+
+	@Override
+	public void registerComponent(
+			IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> component) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unregisterComponent(
+			IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> component) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initComponents(IAction<ActionEvent, Object> action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initComponent(
+			IAction<ActionEvent, Object> action,
+			IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> component) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleAndReplaceComponent(
+			IAction<ActionEvent, Object> action,
+			IPerspective<EventHandler<ActionEvent>, ActionEvent, Object> component) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
 }
