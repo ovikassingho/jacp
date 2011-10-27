ff4c353ca5a6d44673fba961b23576771a4fbe61
afreakm@gmx.de <afreakm@gmx.de@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2011-10-27 19:56:11 +0000

    demo perspective added

---
+++ trunk/JACP.JavaFX2/src/main/java/org/jacp/javafx2/rcp/demo/DempFX2PerspectiveOne.java
@@ -0,0 +1,40 @@
+package org.jacp.javafx2.rcp.demo;
+
+import java.util.Map;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.MenuBar;
+
+import org.jacp.api.action.IAction;
+import org.jacp.api.componentLayout.Layout;
+import org.jacp.javafx2.rcp.componentLayout.FX2PerspectiveLayout;
+import org.jacp.javafx2.rcp.perspective.AFX2Perspective;
+
+/**
+ * Demo perspective class for jacp JavaFX2 implementation
+ * @author Andy Moncsek
+ *
+ */
+public class DempFX2PerspectiveOne extends AFX2Perspective{
+
+	@Override
+	public void handleMenuEntries(MenuBar menuBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleBarEntries(Map<Layout, Node> bars) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handlePerspective(IAction<ActionEvent, Object> action,
+			FX2PerspectiveLayout perspectiveLayout) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
