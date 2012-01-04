8795c43a2702e2c1e903fd7f7b8a1a772d0bf33a
afreakm <afreakm@gmx.de>
2012-01-04 19:03:32 +0000

    update for use with JavaFX2Spring dependency

--- JACP.JavaFX2DemoArea/src/main/java/org/jacp/javafx2/rcp/main/DemoWorkbenchMain.java
+++ JACP.JavaFX2DemoArea/src/main/java/org/jacp/javafx2/rcp/main/DemoWorkbenchMain.java
@@ -1,42 +1,33 @@
 package org.jacp.javafx2.rcp.main;
 
+
+import org.jacp.project.launcher.AFX2SpringLauncher;
+
 import javafx.application.Application;
 import javafx.application.ConditionalFeature;
 import javafx.application.Platform;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import org.jacp.api.launcher.Launcher;
-import org.jacp.api.workbench.IWorkbench;
-import org.jacp.javafx2.rcp.launcher.SpringLauncher;
-import org.jacp.javafx2.rcp.workbench.AFX2Workbench;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class DemoWorkbenchMain extends AFX2SpringLauncher {
 
-public class DemoWorkbenchMain extends Application {
-	final Launcher<ClassPathXmlApplicationContext> launcher = new SpringLauncher(
-			"main.xml");
-
-	private Scene scene;
-
+	public DemoWorkbenchMain() {
+		super("main.xml");
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
 		Application.launch(args);
-
 	}
+	
+
+	private Scene scene;
 
 	@Override
-	public void start(Stage stage) throws Exception {
-		final IWorkbench< Node, EventHandler<Event>, Event, Object> workbench = (IWorkbench< Node, EventHandler<Event>, Event, Object>) launcher
-				.getContext().getBean("workbench");
-		workbench.init(launcher);
-		((AFX2Workbench) workbench).start(stage);
+	public void postInit(Stage stage) {
 		// Adding CSS to the stage
 		boolean is3dSupported = Platform
 				.isSupported(ConditionalFeature.SCENE3D);
