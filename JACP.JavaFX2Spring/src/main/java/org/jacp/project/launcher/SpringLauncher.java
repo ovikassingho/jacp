f197a34dd4da73a0eef7cf196c3f7dcf4dd25780
afreakm <afreakm@gmx.de>
2012-04-01 19:15:52 +0000

    update to Spring 3.1 , postProcessor added to change scope of stateles components to "prototype" automatically

--- JACP.JavaFX2Spring/src/main/java/org/jacp/project/launcher/SpringLauncher.java
+++ JACP.JavaFX2Spring/src/main/java/org/jacp/project/launcher/SpringLauncher.java
@@ -1,29 +1,35 @@
 package org.jacp.project.launcher;
 
 import org.jacp.api.launcher.Launcher;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
- * The SpringLaucher class resolves the spring.xml file and handles access to beans
+ * The SpringLaucher class resolves the spring.xml file and handles access to
+ * beans
+ * 
  * @author Andy Moncsek
- *
+ * 
  */
-public class SpringLauncher  implements Launcher<ClassPathXmlApplicationContext> {
-    private final ClassPathXmlApplicationContext context;
+public class SpringLauncher implements Launcher<ClassPathXmlApplicationContext> {
+	private final ClassPathXmlApplicationContext context;
+	private final ConfigurableListableBeanFactory factory;
+	private final String BASIC_CONFIG_BEANS = "basic.xml";
 
-    public SpringLauncher(final String resource) {
-	context = new ClassPathXmlApplicationContext(new String[] { resource });
-    }
+	public SpringLauncher(final String resource) {
+		this.context = new ClassPathXmlApplicationContext(new String[] {
+				resource, this.BASIC_CONFIG_BEANS });
+		this.factory = this.context.getBeanFactory();
+	}
 
-    @Override
-    public ClassPathXmlApplicationContext getContext() {
-	return context;
-    }
+	@Override
+	public ClassPathXmlApplicationContext getContext() {
+		return this.context;
+	}
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public synchronized <E> E getBean(final Class<E> clazz) {
-	final String[] name = context.getBeanNamesForType(clazz);
-	return (E) context.getBean(name[0]);
-    }
+	@Override
+	public synchronized <E> E getBean(final Class<E> clazz) {
+		return this.factory.getBean(clazz);
+	}
 
 }
