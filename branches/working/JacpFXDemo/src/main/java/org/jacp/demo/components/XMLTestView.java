17951484b9eb0e1b2e1d62faa637e0cd9d261636
amo.ahcp@googlemail.com <amo.ahcp@googlemail.com@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2012-05-20 20:05:49 +0000

    Test integration of a FXML component

---
+++ branches/working/JacpFXDemo/src/main/java/org/jacp/demo/components/XMLTestView.java
@@ -0,0 +1,68 @@
+package org.jacp.demo.components;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import org.jacp.api.action.IAction;
+import org.jacp.api.annotations.DeclarativeComponent;
+import org.jacp.api.annotations.OnStart;
+import org.jacp.javafx.rcp.component.AFXMLComponent;
+import org.jacp.javafx.rcp.componentLayout.FXComponentLayout;
+import org.jacp.javafx.rcp.util.FXUtil;
+import org.jacp.javafx.rcp.util.FXUtil.MessageUtil;
+
+@DeclarativeComponent(defaultExecutionTarget = "PmainContentTop", id = "id006", name = "XMLTestView", active = true, uiDescriptionFile = "/AdoptionForm.fxml")
+public class XMLTestView extends AFXMLComponent {
+	@FXML
+	private GridPane grid;
+	@FXML
+	private TextField field1;
+
+	@FXML
+	private void handleSubmit(ActionEvent event) {
+		
+		System.out.println("action : "+ grid);
+		grid.setGridLinesVisible(!grid.isGridLinesVisible());
+	}
+
+	@Override
+	public Node handleAction(IAction<Event, Object> action) {
+		System.out.println("handle : "+ grid+ " "+action.getLastMessage()+" thread"+ Thread.currentThread());
+		return null;
+	}
+
+	@Override
+	public void postHandleAction(Node xmlParent, Node handleNode,
+			IAction<Event, Object> action) {
+		// TODO Auto-generated method stub
+		System.out.println("postHandle : "+ grid+ " "+action.getLastMessage()+" thread"+ Thread.currentThread());
+		if(!action.getLastMessage().equals(MessageUtil.INIT)) field1.setText(action.getLastMessage().toString());
+
+	}
+	@OnStart
+	public void start(FXComponentLayout layout,URL url, ResourceBundle resourceBundle) {
+		System.out.println("STRAT" + grid+"  "+ layout+" url: "+ url.getPath());
+	}
+	
+//	@OnStart
+//	public void start(FXComponentLayout layout) {
+//		System.out.println("STRAT" + grid+"  "+ layout);
+//	}
+
+//	@OnStart
+//	public void start(URL url, ResourceBundle resourceBundle) {
+//		System.out.println("STRAT" + grid+"  "+ " url: "+ url.getPath());
+//	}
+	
+//	@OnStart
+//	public void start(URL url) {
+//		System.out.println("STRAT" + grid+"  ");
+//	}
+}
