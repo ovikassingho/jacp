67ff704e39eeac999fd9308847b6619bc74f6bdd
aushilfselch@yahoo.de <aushilfselch@yahoo.de@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2012-08-30 11:44:46 +0000

    resourceBundle for dialog buttons!

--- branches/working/JACP.JavaFXControls/src/main/java/org/jacp/javafx/rcp/controls/optionPane/JACPDialogButton.java
+++ branches/working/JACP.JavaFXControls/src/main/java/org/jacp/javafx/rcp/controls/optionPane/JACPDialogButton.java
@@ -22,6 +22,8 @@
  ************************************************************************/
 package org.jacp.javafx.rcp.controls.optionPane;
 
+import org.jacp.javafx.rcp.controls.util.ResourceBundleUtil;
+
 /**
  * The Enum JACPDialogButton.
  * 
@@ -29,99 +31,100 @@ package org.jacp.javafx.rcp.controls.optionPane;
  */
 public enum JACPDialogButton {
 
-	/** The OK-Button. */
-	OK(1, "OK"),
-	/** The CANCEL-Button. */
-	CANCEL(2, "Cancel"),
-	/** The YES-Button. */
-	YES(3, "Yes"),
-	/** The NO-Button. */
-	NO(4, "No");
+    /** The OK-Button. */
+    OK(1, "btn.dialog.ok"),
+    /** The CANCEL-Button. */
+    CANCEL(2, "btn.dialog.cancel"),
+    /** The YES-Button. */
+    YES(3, "btn.dialog.yes"),
+    /** The NO-Button. */
+    NO(4, "btn.dialog.no");
 
-	/** The id. */
-	private int id;
+    /** The id. */
+    private int id;
 
-	/** The label. */
-	private String label;
+    /** The label. */
+    private String label;
 
-	/**
-	 * Instantiates a new jACP dialog button.
-	 * 
-	 * @param id
-	 *            the id
-	 * @param label
-	 *            the label
-	 */
-	JACPDialogButton(final int id, final String label) {
-		this.id = id;
-		this.label = label;
-	}
+    /**
+     * Instantiates a new jACP dialog button.
+     * 
+     * @param id
+     *            the id
+     * @param label
+     *            the label
+     */
+    JACPDialogButton(final int id, final String label) {
+        this.id = id;
+        this.label = label;
+    }
 
-	/**
-	 * From id.
-	 * 
-	 * @param id
-	 *            the id
-	 * @return the jACP dialog button
-	 */
-	public static JACPDialogButton fromId(final int id) {
-		JACPDialogButton currentButton = JACPDialogButton.OK;
-		switch (id) {
-		case 1:
-			currentButton = JACPDialogButton.OK;
-			break;
-		case 2:
-			currentButton = JACPDialogButton.CANCEL;
-			break;
-		case 3:
-			currentButton = JACPDialogButton.YES;
-			break;
-		case 4:
-			currentButton = JACPDialogButton.NO;
-			break;
-		default:
-			currentButton = JACPDialogButton.OK;
-			break;
-		}
-		return currentButton;
-	}
+    /**
+     * From id.
+     * 
+     * @param id
+     *            the id
+     * @return the jACP dialog button
+     */
+    public static JACPDialogButton fromId(final int id) {
+        JACPDialogButton currentButton = JACPDialogButton.OK;
+        switch (id) {
+        case 1:
+            currentButton = JACPDialogButton.OK;
+            break;
+        case 2:
+            currentButton = JACPDialogButton.CANCEL;
+            break;
+        case 3:
+            currentButton = JACPDialogButton.YES;
+            break;
+        case 4:
+            currentButton = JACPDialogButton.NO;
+            break;
+        default:
+            currentButton = JACPDialogButton.OK;
+            break;
+        }
+        return currentButton;
+    }
 
-	/**
-	 * Gets the id.
-	 * 
-	 * @return the id
-	 */
-	public int getId() {
-		return this.id;
-	}
+    /**
+     * Gets the id.
+     * 
+     * @return the id
+     */
+    public int getId() {
+        return this.id;
+    }
 
-	/**
-	 * Sets the id.
-	 * 
-	 * @param id
-	 *            the new id
-	 */
-	public void setId(final int id) {
-		this.id = id;
-	}
+    /**
+     * Sets the id.
+     * 
+     * @param id
+     *            the new id
+     */
+    public void setId(final int id) {
+        this.id = id;
+    }
 
-	/**
-	 * Gets the label.
-	 * 
-	 * @return the label
-	 */
-	public String getLabel() {
-		return this.label;
-	}
+    /**
+     * Gets the label.
+     * 
+     * @return the label
+     */
+    public String getLabel() {
+//        return this.label;
+        return ResourceBundleUtil.getBundle().getString(this.label);
+    }
 
-	/**
-	 * Sets the label.
-	 * 
-	 * @param label
-	 *            the new label
-	 */
-	public void setLabel(final String label) {
-		this.label = label;
-	}
+    /**
+     * Sets the label.
+     * 
+     * @param label
+     *            the new label
+     */
+    public void setLabel(final String label) {
+        this.label = label;
+    }
 
 }
