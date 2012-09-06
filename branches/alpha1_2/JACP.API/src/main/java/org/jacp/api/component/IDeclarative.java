7f192149305e5bc7aa630fd0f9bc87b66c84867b
amo.ahcp@gmail.com <amo.ahcp@gmail.com@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2012-09-06 08:52:49 +0000

    create alpha 1.2

---
+++ branches/alpha1_2/JACP.API/src/main/java/org/jacp/api/component/IDeclarative.java
@@ -0,0 +1,80 @@
+/************************************************************************
+ * 
+ * Copyright (C) 2010 - 2012
+ *
+ * [IVComponent.java]
+ * AHCP Project (http://jacp.googlecode.com)
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ *
+ ************************************************************************/
+package org.jacp.api.component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.jacp.api.util.UIType;
+
+/**
+ * Declarative Components always have a document URL and should have an resourceBundle.
+ * 
+ * @author Andy Moncsek
+ */
+public interface IDeclarative {
+	/**
+	 * Contains the document url describing the UI.
+	 * 
+	 * @return the document url
+	 */
+	String getViewLocation();
+
+	/**
+	 * Set the viewLocation location on component start.
+	 * 
+	 * @param documentURL
+	 */
+	void setViewLocation(String documentURL);
+
+	/**
+	 * The document URL describing the UI.
+	 * 
+	 * @return the document url
+	 */
+	URL getDocumentURL();
+	
+	/**
+	 * Contains locale-specific objects.
+	 * 
+	 * @return the resource bundle for the UI document
+	 */
+	ResourceBundle getResourceBundle();
+	
+	/**
+	 * Distinguish component types.
+	 * @return the type of the component.
+	 */
+	UIType getType();
+	
+	/**
+	 * Represents the Locale ID, see: http://www.oracle.com/technetwork/java/javase/locales-137662.html
+	 * @return
+	 */
+	String getLocaleID();
+	/**
+	 * Represents the location of your resource bundle file.
+	 * @return
+	 */
+	String getResourceBundleLocation();
+}
