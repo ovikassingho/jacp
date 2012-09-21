a3834965345e92cc6d69f31c4facd270d8e5cb8b
afreakm@gmx.de <afreakm@gmx.de@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2012-09-21 13:52:30 +0000

    freeze to observe bug

---
+++ branches/alpha1_2_1/JACP.JavaFX/src/main/java/org/jacp/javafx/rcp/util/CSSUtil.java
@@ -0,0 +1,48 @@
+/************************************************************************
+ * 
+ * Copyright (C) 2010 - 2012
+ *
+ * [CSSUtil.java]
+ * AHCP Project http://jacp.googlecode.com
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ *
+ ************************************************************************/
+package org.jacp.javafx.rcp.util;
+
+import javafx.scene.Node;
+
+public class CSSUtil {
+
+    public class CSSConstants {
+        // CLASSES
+        public static final String CLASS_DARK_BORDER = "dark-border";
+        public static final String CLASS_WINDOW_BUTTONS = "window-buttons";
+        public static final String CLASS_JACP_TOOL_BAR = "jacp-tool-bar";
+        public static final String CLASS_JACP_BUTTON_BAR = "jacp-button-bars";
+
+        // IDS
+        public static final String ID_WINDOW_MIN = "window-min";
+        public static final String ID_WINDOW_MAX = "window-max";
+        public static final String ID_WINDOW_CLOSE = "window-close";
+        public static final String ID_ROOT = "root";
+        public static final String ID_ROOT_PANE = "root-pane";
+    }
+
+    public static void setBackgroundColor(Node node, String color) {
+        node.setStyle("-fx-background-color:" + color);
+    }
+
+}
