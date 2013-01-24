a70b811b6d1c731841c0593dad5e48ab082f567d
amo.ahcp@gmail.com <amo.ahcp@gmail.com@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2013-01-24 21:02:34 +0000

    see changelog

--- branches/working/JACP.API/src/main/java/org/jacp/api/annotations/Dialog.java
+++ branches/working/JACP.API/src/main/java/org/jacp/api/annotations/Dialog.java
@@ -27,33 +27,53 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.jacp.api.dialog.Scope;
+
 /**
- * Defines the meta attributes for a callback component.
+ * Defines a managed dialog component, A dialog has a parent component
+ * accessible by @Ressource annotation, the Dialog should either extend a Node
+ * or define a viewLocation pointing to fxml.
  * 
  * @author Andy Moncsek
  * 
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface CallbackComponent {
-	/**
-	 * The components name.
-	 * 
-	 * @return The component name.
-	 */
-	String name();
-
+public @interface Dialog {
 	/**
 	 * The component id.
 	 * 
-	 * @return The component Id.
+	 * @return The component Id
 	 */
 	String id();
 
 	/**
-	 * The active state at start time.
+	 * Defines the Scope of the Dialog, default is Singleton.
 	 * 
-	 * @return True
+	 * @return The dialog {@link Scope}
 	 */
-	boolean active() default true;
+	Scope scope() default Scope.SINGLETON;
+
+	/**
+	 * Represents the location (URI) of the declarative UI.
+	 * 
+	 * @return The view location (like bundle.messages)
+	 */
+	String viewLocation() default "";
+
+	/**
+	 * Represents the location of your resource bundle file.
+	 * 
+	 * @return The default resource bundle location (like bundle.messages)
+	 */
+	String resourceBundleLocation() default "";
+	
+
+    /**
+     * Represents the Locale ID. see:
+     * http://www.oracle.com/technetwork/java/javase/locales-137662.html
+     * 
+     * @return The default locale Id
+     */
+    String localeID() default "";
 }
