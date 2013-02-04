421b33b898494fa5c79a82d7c571abf1bd58db8d
aushilfselch <aushilfselch@yahoo.de>
2013-02-04 20:54:38 +0000

    Utils and CSS-fix

--- JACP.JavaFX/src/main/java/org/jacp/javafx/rcp/util/CSSUtil.java
+++ JACP.JavaFX/src/main/java/org/jacp/javafx/rcp/util/CSSUtil.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2010 - 2013
  *
  * [CSSUtil.java]
- * AHCP Project http://jacp.googlecode.com
+ * AHCP Project (http://jacp.googlecode.com)
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,23 +26,88 @@ import javafx.scene.Node;
 
 public class CSSUtil {
 
-    public class CSSConstants {
+    public interface CSSConstants {
         // CLASSES
-        public static final String CLASS_DARK_BORDER = "dark-border";
-        public static final String CLASS_WINDOW_BUTTONS = "window-buttons";
-        public static final String CLASS_JACP_TOOL_BAR = "jacp-tool-bar";
-        public static final String CLASS_JACP_BUTTON_BAR = "jacp-button-bars";
+        static final String CLASS_DARK_BORDER               = "dark-border";
+        static final String CLASS_WINDOW_BUTTONS            = "window-buttons";
+        static final String CLASS_JACP_TOOL_BAR             = "jacp-tool-bar";
+        static final String CLASS_JACP_BUTTON_BAR           = "jacp-button-bars";
+        
+        // BIS
+        static final String CLASS_BIS_IMAGE_SECTION         = "ic-bis-image-section";
+        static final String CLASS_BIS_PRODUCT_SECTION       = "ic-bis-product-section";
+        static final String CLASS_BIS_PRODUCT_ITEM          = "ic-bis-product-item";
+        static final String CLASS_BIS_IMAGE_TOOLBAR         = "ic-bis-image-toolbar";
 
         // IDS
-        public static final String ID_WINDOW_MIN = "window-min";
-        public static final String ID_WINDOW_MAX = "window-max";
-        public static final String ID_WINDOW_CLOSE = "window-close";
-        public static final String ID_ROOT = "root";
-        public static final String ID_ROOT_PANE = "root-pane";
+        static final String ID_WINDOW_MIN                   = "window-min";
+        static final String ID_WINDOW_MAX                   = "window-max";
+        static final String ID_WINDOW_CLOSE                 = "window-close";
+        static final String ID_ROOT                         = "root";
+        static final String ID_ROOT_PANE                    = "root-pane";
+    }
+    
+    
+    public interface GlobalValues {
+        
+        public static final double SINGLE_PADDING = 20;
+        
     }
 
+    /**
+     * Sets the background color style to a given node.
+     *
+     * @param node the node
+     * @param color the color
+     */
     public static void setBackgroundColor(Node node, String color) {
         node.setStyle("-fx-background-color:" + color);
+    }
+
+    /**
+     * Sets the background color to one ore more nodes.
+     *
+     * @param color the color
+     * @param node the node
+     */
+    public static void setBackgroundColors(String color, Node... nodes) {
+        for (final Node node : nodes) {
+            setBackgroundColor(node, color);
+        }
+    }
+
+    /**
+     * Adds the css class to one or more nodes. 
+     *
+     * @param className the class name
+     * @param nodes the nodes
+     */
+    public static void addCSSClass(final String className, Node... nodes) {
+        for (final Node node : nodes) {
+            node.getStyleClass().add(className);
+        }
+    }
+
+    /**
+     * Removes a css class from one or more nodes.
+     *
+     * @param className the class name
+     * @param nodes the nodes
+     */
+    public static void removeCSSClass(final String className, Node... nodes) {
+        for (final Node node : nodes) {
+            node.getStyleClass().remove(className);
+        }
+    }
+
+    /**
+     * Removes all given css classes from one node. 
+     *
+     * @param node the node
+     * @param classNames the class names
+     */
+    public static void removeCSSClasses(Node node, String... classNames) {
+        node.getStyleClass().remove(classNames);
     }
 
 }
--- JACP.JavaFX/src/main/java/org/jacp/javafx/rcp/util/LayoutUtil.java
+++ JACP.JavaFX/src/main/java/org/jacp/javafx/rcp/util/LayoutUtil.java
@@ -1,7 +1,12 @@
 package org.jacp.javafx.rcp.util;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
 /**
@@ -21,6 +26,48 @@ public class LayoutUtil {
             }
         }
 
+        
+        public static void setHGrow(Priority priority, Node... nodes) {
+            for (Node node : nodes) {
+                if (node != null) {
+                    GridPane.setHgrow(node, priority);
+                }
+            }
+        }
+        
+        
+        public static void setVGrow(Priority priority, Node... nodes) {
+            for (Node node : nodes) {
+                if (node != null) {
+                    GridPane.setVgrow(node, priority);
+                }
+            }
+        }
+        
     }
 
+    public static class HBoxUtil {
+
+        public static void setHGrow(Priority priority, Node... nodes) {
+            for (Node node : nodes) {
+                if (node != null) {
+                    HBox.setHgrow(node, priority);
+                }
+            }
+        }
+
+        public static void setMargin(Insets insets, Node... nodes) {
+            setMargin(insets, Arrays.asList(nodes));
+
+        }
+
+        public static void setMargin(Insets insets, Collection<Node> nodes) {
+            for (Node node : nodes) {
+                if (node != null) {
+                    HBox.setMargin(node, insets);
+                }
+            }
+        }
+
+    }
 }
