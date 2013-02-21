437de77b1b904bc54da93c599e316743623c85c9
amo.ahcp@googlemail.com <amo.ahcp@googlemail.com@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2013-02-21 20:07:37 +0000

    move Version 1 to tags

---
+++ tags/Versions/JacpFX1.0/JACP.JavaFX2/src/main/java/org/jacp/javafx2/rcp/componentLayout/FX2WorkbenchLayout.java
@@ -0,0 +1,128 @@
+/************************************************************************
+ * 
+ * Copyright (C) 2010 - 2012
+ *
+ * [FX2WorkbenchLayout.java]
+ * AHCP Project (http://jacp.googlecode.com)
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ *
+ ************************************************************************/
+package org.jacp.javafx2.rcp.componentLayout;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import javafx.scene.Node;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.Pane;
+import javafx.stage.StageStyle;
+
+import org.jacp.api.componentLayout.IWorkbenchLayout;
+import org.jacp.api.util.ToolbarPosition;
+import org.jacp.api.util.Tupel;
+import org.jacp.javafx2.rcp.components.toolBar.JACPToolBar;
+
+/**
+ * defines basic layout of workbench; define if menus are enabled; declare tool
+ * bars; set workbench size
+ * 
+ * @author Andy Moncsek
+ */
+public class FX2WorkbenchLayout implements IWorkbenchLayout<Node> {
+
+	private boolean menueEnabled;
+	private final Tupel<Integer, Integer> size = new Tupel<Integer, Integer>();
+	private final Map<ToolbarPosition, ToolBar> registeredToolbars = new TreeMap<ToolbarPosition, ToolBar>();
+	private MenuBar menu;
+	private Pane glassPane;
+	private StageStyle style = StageStyle.DECORATED;
+
+	@Override
+	public boolean isMenuEnabled() {
+		return this.menueEnabled;
+	}
+
+	@Override
+	public void setMenuEnabled(boolean enabled) {
+		this.menueEnabled = enabled;
+		if (enabled && this.menu == null) {
+			this.menu = new MenuBar();
+			this.menu.setId("main-menu");
+		}
+	}
+
+	@Override
+	public void setWorkbenchXYSize(int x, int y) {
+		this.size.setX(x);
+		this.size.setY(y);
+	}
+
+	@Override
+	public Tupel<Integer, Integer> getWorkbenchSize() {
+		return this.size;
+	}
+
+	private ToolBar initToolBar(ToolbarPosition position) {
+		final JACPToolBar bar = new JACPToolBar();
+		bar.setId(position.getName() + "-bar");
+		return bar;
+	}
+
+	@Override
+	public void registerToolBar(ToolbarPosition position) {
+		registeredToolbars.put(position, initToolBar(position));
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public <S extends Enum> void setStyle(S style) {
+		this.style = (StageStyle) style;
+
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public <S extends Enum> S getStyle() {
+		return (S) this.style;
+	}
+
+	@Override
+	public MenuBar getMenu() {
+		return this.menu;
+	}
+
+	/**
+	 * Gets the registered toolbars.
+	 * 
+	 * @return the registered toolbars
+	 */
+	public Map<ToolbarPosition, ToolBar> getRegisteredToolbars() {
+		return registeredToolbars;
+	}
+
+	@Override
+	public ToolBar getRegisteredToolBar(ToolbarPosition position) {
+		return registeredToolbars.get(position);
+	}
+
+	public Pane getGlassPane() {
+		if (glassPane == null)
+			glassPane = new Pane();
+		return glassPane;
+	}
+
+}
---
+++ tags/Versions/JacpFX1.0/JacpFXDemo/src/main/java/org/jacp/demo/callbacks/CoordinatorCallback.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2010 - 2012.
+ * AHCP Project (http://code.google.com/p/jacp)
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.jacp.demo.callbacks;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+import org.jacp.api.action.IAction;
+import org.jacp.api.action.IActionListener;
+import org.jacp.demo.entity.Contact;
+import org.jacp.demo.entity.ContactDTO;
+import org.jacp.demo.main.Util;
+import org.jacp.javafx2.rcp.component.AStatelessCallbackComponent;
+
+/**
+ * The coordinatorCallback splits the amount of contacts in to chunks and sends
+ * it to the coordinatorCallback
+ * 
+ * @author Andy Moncsek
+ * 
+ */
+public class CoordinatorCallback extends AStatelessCallbackComponent {
+
+	@Override
+	public Object handleAction(final IAction<Event, Object> action) {
+		if (action.getLastMessage() instanceof Contact) {
+			final Contact contact = (Contact) action.getLastMessage();
+			if (contact.getContacts().isEmpty()) {
+				if (contact.getAmount() > Util.PARTITION_SIZE) {
+					int amount = contact.getAmount();
+					while (amount > Util.PARTITION_SIZE) {
+						this.waitAmount(100);
+						this.createAmountAndSend(contact.getFirstName(),
+								Util.PARTITION_SIZE);
+						amount = amount - Util.PARTITION_SIZE;
+					}
+					this.createAmountAndSend(contact.getFirstName(), amount);
+				} else {
+					this.createAmountAndSend(contact.getFirstName(),
+							contact.getAmount());
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * for demo purposes
+	 */
+	private void waitAmount(final int amount) {
+		try {
+			Thread.sleep(amount);
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * create a contact dto with parent name and amount of contacts to create,
+	 * send dto to creator callback
+	 * 
+	 * @param name
+	 * @param amount
+	 */
+	private void createAmountAndSend(final String name, final int amount) {
+		final ContactDTO dto = new ContactDTO(name, amount);
+		final IActionListener<EventHandler<Event>, Event, Object> listener = this
+				.getActionListener("id01.id005", dto);
+		listener.performAction(null);
+	}
+
+}
