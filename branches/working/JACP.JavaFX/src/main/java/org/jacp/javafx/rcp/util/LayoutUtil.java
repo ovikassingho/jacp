58e07a29f4fbedbf26f4dc14d3c7e52cfce56616
pete.jacp@googlemail.com <pete.jacp@googlemail.com@285587e2-1dac-d1f2-68de-6a3a92e3921a>
2013-05-30 13:02:13 +0000

    CSSUtil Background-color

--- branches/working/JACP.JavaFX/src/main/java/org/jacp/javafx/rcp/util/LayoutUtil.java
+++ branches/working/JACP.JavaFX/src/main/java/org/jacp/javafx/rcp/util/LayoutUtil.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -116,6 +117,56 @@ public class LayoutUtil {
             for (Node node : nodes) {
                 if (node != null) {
                     HBox.setMargin(node, insets);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * The VBoxUtil subclass.
+     *
+     */
+    public static class VBoxUtil {
+
+        /**
+         *
+         * Set VBox vGrow to multiple Nodes
+         *
+         * @param priority - priority to set
+         * @param nodes - the nodes
+         */
+        public static void setVGrow(Priority priority, Node... nodes) {
+            for (Node node : nodes) {
+                if (node != null) {
+                    VBox.setVgrow(node, priority);
+                }
+            }
+        }
+
+        /**
+         *
+         * Set margin to multiple Nodes.
+         *
+         * @param insets - the margin insets
+         * @param nodes - the nodes to receive the margin
+         */
+        public static void setMargin(Insets insets, Node... nodes) {
+            setMargin(insets, Arrays.asList(nodes));
+
+        }
+
+        /**
+         *
+         * Set margin to a collection of Nodes.
+         *
+         * @param insets - the margin insets
+         * @param nodes - the nodes to receive the margin
+         */
+        public static void setMargin(Insets insets, Collection<Node> nodes) {
+            for (Node node : nodes) {
+                if (node != null) {
+                    VBox.setMargin(node, insets);
                 }
             }
         }
